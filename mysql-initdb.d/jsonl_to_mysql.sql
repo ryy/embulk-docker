563c2978fd9a104da7f5af392d1e543481707043
ryo.naruse <naruse19910801@gmail.com>
2021-07-31 13:33:17 +0900

    Add parse jsonl

---
+++ mysql-initdb.d/jsonl_to_mysql.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE IF NOT EXISTS embulk;
+
+use embulk;
+
+CREATE TABLE IF NOT EXISTS jsonl_to_mysql
+(
+  `id` bigint NOT NULL PRIMARY KEY,
+  `str` varchar(255) DEFAULT NULL,
+  `num` bigint DEFAULT NULL,
+  `bool` boolean DEFAULT NULL,
+  `time_at` timestamp DEFAULT NULL,
+);
