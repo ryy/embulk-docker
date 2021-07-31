a90c526f0e53d4bcc3e8131446c4de356236eefa
ryo.naruse <naruse19910801@gmail.com>
2021-07-31 10:47:53 +0900

    Update csv_to_mysql

--- mysql-initdb.d/csv_to_mysql.sql
+++ mysql-initdb.d/csv_to_mysql.sql
@@ -5,7 +5,7 @@ use embulk;
 CREATE TABLE IF NOT EXISTS csv_to_mysql
 (
   `id` bigint NOT NULL PRIMARY KEY,
-  `account` text DEFAULT NULL,
+  `account` bigint DEFAULT NULL,
   `time` timestamp NULL DEFAULT NULL,
   `purchase` timestamp NULL DEFAULT NULL,
   `comment` varchar(255) DEFAULT NULL
